f572f1c85982dd502269b4a3bf2c7fe15056fedf
didprojects <118237637+didi-MACLU@users.noreply.github.com>
2023-03-20 19:51:54 +1300

    version1-three types of albums

--- src/App.js
+++ src/App.js
@@ -1,23 +1,40 @@
 import logo from './logo.svg';
 import './App.css';
+import ShowImages from './ShowImages';
 
 function App() {
+
+  const pets = [
+    "https://cdn.pixabay.com/photo/2017/09/25/13/12/puppy-2785074_1280.jpg",
+    "https://cdn.pixabay.com/photo/2018/03/31/06/31/dog-3277416_1280.jpg",
+    "https://cdn.pixabay.com/photo/2016/10/31/14/55/rottweiler-1785760_1280.jpg",
+    "https://cdn.pixabay.com/photo/2018/10/01/09/21/pets-3715733_1280.jpg",
+    "https://cdn.pixabay.com/photo/2016/10/15/12/01/dog-1742295_1280.jpg"
+];
+  const beaches = [
+    "https://lp-cms-production.imgix.net/2021-07/Waya_Island_Fiji.jpg?auto=format&q=75&w=3840",
+    "https://cdn.mos.cms.futurecdn.net/wtqqnkYDYi2ifsWZVW2MT4-1920-80.jpg.webp",
+    "https://www.scti.co.nz/-/media/project/scti/nz/images/travel-advice/fijis-best-beaches/fijis-best-beaches-musket-cove-malolo-lailai-900x675.jpg",
+    "https://lp-cms-production.imgix.net/2022-03/GettyImages-1022108932.jpg?auto=format&q=75&w=3840",
+    "https://img.theculturetrip.com/1440x807/smart/wp-content/uploads/2021/05/2c4j3en-e1625485939998.jpg"
+  ];
+
+  const foods = [
+    "https://images.pexels.com/photos/1092730/pexels-photo-1092730.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2",
+    "https://images.pexels.com/photos/566566/pexels-photo-566566.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2",
+    "https://images.pexels.com/photos/1099680/pexels-photo-1099680.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2",
+    "https://images.pexels.com/photos/361184/asparagus-steak-veal-steak-veal-361184.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2",
+    "https://images.pexels.com/photos/769969/pexels-photo-769969.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2"
+];
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+      <header className="app-header">
+        <h2 className='header-content'>My Album</h2>
       </header>
+      <div>
+        <ShowImages pets={pets} beaches={beaches} foods={foods}/>
+      </div>
     </div>
   );
 }
---
+++ src/ShowImages.js
@@ -0,0 +1,78 @@
+import "./ShowImages.css"
+import { useState } from "react";
+
+function ShowImages(props){
+    const pets = props.pets;
+    const beaches = props.beaches;
+    const foods = props.foods;
+
+    const [current,setCurrent] = useState(0)
+    const [images,setImages] = useState(beaches)
+
+    function passBeachImages(){
+        setImages(beaches);
+        setCurrent(0);
+    }
+
+    function passPetsImages(){
+        setImages(pets);
+        setCurrent(0);
+    }
+    function passFoodImages(){
+        setImages(foods);
+        setCurrent(0);
+    }
+
+    function nextSlide(){
+        setCurrent(current === images.length-1? 0:current+1);
+    }
+
+    function prevSlide(){
+        setCurrent(current === 0? images.length-1:current-1);
+    }
+
+    
+    return (
+        <div className="columns">
+            <div className="primary">
+                <div className="slider">
+                        <div className="left-arrow" onClick={prevSlide}>⬅</div>
+                        <div className="right-arrow" onClick={nextSlide}>⮕</div>
+                </div>
+                <div>
+                    {images.map(
+                        (image,index)=>
+                            current===index && (
+                                <div className="slide">
+                                    <img src={image} className="slide-image" alt="images" />
+                                </div>
+                            )
+                    )}
+                </div>
+            </div>
+
+            <div className="secondary">
+                <div className="navi">
+                    <div className="image-list1" onClick={passBeachImages}>
+                        <img src={beaches[0]} alt="beach images" width="180px" height="120px"></img>
+                        <h3>Beach</h3>
+                        <br/>
+                    </div>
+                    <div className="image-list2" onClick={passPetsImages}>
+                        <img src={pets[0]} alt="pets images" width="180px" height="120px"></img>
+                        <h3>Pets</h3>
+                        <br/>
+                    </div>
+                    <div className="image-list3" onClick={passFoodImages}>
+                        <img src={foods[0]} alt="food images" width="180px" height="120px"></img>
+                        <h3>Food</h3>
+                    </div>
+                </div>
+            </div>
+
+            
+        </div>
+    )
+}
+
+export default ShowImages;
